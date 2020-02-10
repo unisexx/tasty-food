822f44db842e4fb53de1f0cd9a42a5bb925f8d30
unisexx <unisexx@gmail.com>
2020-02-10 20:25:39 +0700

    อัพเดทหน้าแรก

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ use App\Models\ProductItem;
 use App\Models\Promotion;
 use App\User;
 use Auth;
+use DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -33,8 +34,44 @@ class HomeController extends Controller
     {
         $hilights = Hilight::where('status', 1)->orderBy('id', 'desc')->get();
         $product_categories = ProductCategory::whereNull('parent_id')->where('status', 1)->orderBy('_lft')->get();
+
+        // สินค้าใหม่
+        $newitems = ProductItem::where('status', 1)->orderBy('id', 'desc')->take(8)->get();
+
+        // สินค้าขายดี
+        $sql = "SELECT
+                    product_items.id,
+                    -- product_items.brand,
+                    -- product_items.`name`,
+                    -- product_items.price,
+                    -- product_items.vip_price,
+                    -- Sum(order_details.price) AS total_price,
+                    Sum(order_details.qty) AS total_qty
+                    FROM
+                        orders
+                    INNER JOIN order_details ON orders.id = order_details.order_id
+                    INNER JOIN product_items ON product_items.id = order_details.product_item_id
+                    WHERE
+                        orders.`status` = 'จัดส่งสินค้าแล้ว'
+                    GROUP BY
+                        product_item_id
+                    ORDER BY
+                        total_qty DESC
+                    LIMIT 8";
+        $rs = DB::select($sql);
+
+        $list_id = [];
+        foreach($rs as $key => $value)
+        {
+            $list_id[] = $value->id;
+        }
+
+        $bestitems = ProductItem::whereIn('id', $list_id)->get();
+
+        // โปรโมชั่น
         $promotions = Promotion::where('status', 1)->inRandomOrder()->take(3)->get();
-        return view('home', compact('hilights', 'product_categories', 'promotions'));
+
+        return view('home', compact('hilights', 'product_categories', 'promotions', 'newitems', 'bestitems'));
     }
 
     public function flogin()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,18 +36,93 @@
     </div>
 </div>
 <!--########################### END SLIDE ###########################-->
-<!--########################## START BANNER PRODUCT ##########################-->
+<!--########################## START WELCOME ##########################-->
 <div class="row m-0">
+    <div class="container mt-5">
+        <div class="title-welcome-home">ยินดีต้อนรับสู่ ร้าน CHC</div>
+        <div class="text-center"><img src="{{ url('chc/images/line-white.jpg') }}" alt=""></div>
+        <p class="title2-welcome-home">ทางร้านจำหน่ายสินค้าอาหารเสริม วิตามิน เวชสำอางราคาถูก ทั้งปลีกและส่ง ทุกรายการสินค้าเป็นของแท้แน่นอน</p>
+        <img src="{{ url('chc/images/banner-home-01.jpg') }}" alt="" class="mt-4 mb-4">
+        <p class="title3-welcome-home">ทางร้านจัดส่งสินค้าทุกวัน...<br>
+            และแจ้งเลขพัสดุให้ทุกท่านก่อนเวลา...</p>
+    <div class="text-center">
+        <img src="{{ url('chc/images/icon-van.png') }}" alt="" class="pr-2"><span class="title5-welcome-home">ส่งจริง ส่งไว ได้ของชัวร์</span>
+        <img src="{{ url('chc/images/icon-phone.png') }}" alt="" class="pl-4 pr-2"><span class="title6-welcome-home">081-1234567</span>
+        <img src="{{ url('chc/images/icon-line.png') }}" alt="" class="pl-4 pr-2"><span class="title6-welcome-home">@chc</span>
+    </div>
+
+    </div>
+</div>
+<!--########################### END WELCOME ###########################-->
+<!--########################## START สินค้ามาใหม่ ##########################-->
+<div class="row m-0">
+    <div class="container mt-5">
+        <div class="title4-welcome-home">สินค้ามาใหม่</div>
+        <div class="row mx-auto my-auto products">
+
+        @foreach($newitems as $product_item)
+            <div class="col-12 col-sm-6 col-md-3 d-inline-block box-products">
+                <p class="name-product">{{ $product_item->name }}</p>
+            <img class="img-fluid" src="{{ url('uploads/product-item/'.@$product_item->productImageFirst->name) }}">
+                <div class="link">
+                    <ul>
+                        <li>
+                            <div class="buy add1 item_add" data-id="{{ $product_item->id }}" data-qty="1">
+                                <img src="{{ url('chc/images/icon-cart.png') }}" alt="" class="icon-buy"> <span class="text-white">Buy</span> <div class="carousel-control-next-icon i-buy"></div></a>
+                            </div>
+                        </li>
+                        <li> <a class="go-detail" href="{{ url('product-item/'.$product_item->id) }}"><img src="{{ url('chc/images/icon-go-detail.png') }}" alt=""> Detail</a></li>
+                    </ul>
+                </div>
+                <p class="price">{{ show_price($product_item) }}.-</p>
+            </div>
+        @endforeach
+        
+    </div>
+
+    </div>
+</div>
+<!--########################### END สินค้ามาใหม่ ###########################-->
+<!--########################## START สินค้าขายดี ##########################-->
+<div class="row m-0">
+    <div class="container mt-5">
+        <div class="title4-welcome-home">สินค้าขายดี</div>
+        <div class="row mx-auto my-auto products">
+
+            @foreach($bestitems as $product_item)
+                <div class="col-12 col-sm-6 col-md-3 d-inline-block box-products">
+                    <p class="name-product">{{ $product_item->name }}</p>
+                <img class="img-fluid" src="{{ url('uploads/product-item/'.@$product_item->productImageFirst->name) }}">
+                    <div class="link">
+                        <ul>
+                            <li>
+                                <div class="buy add1 item_add" data-id="{{ $product_item->id }}" data-qty="1">
+                                    <img src="{{ url('chc/images/icon-cart.png') }}" alt="" class="icon-buy"> <span class="text-white">Buy</span> <div class="carousel-control-next-icon i-buy"></div></a>
+                                </div>
+                            </li>
+                            <li> <a class="go-detail" href="{{ url('product-item/'.$product_item->id) }}"><img src="{{ url('chc/images/icon-go-detail.png') }}" alt=""> Detail</a></li>
+                        </ul>
+                    </div>
+                    <p class="price">{{ show_price($product_item) }}.-</p>
+                </div>
+            @endforeach
+
+        </div>
+    </div>
+</div>
+<!--########################### END สินค้าขายดี ###########################-->
+<!--########################## START BANNER PRODUCT ##########################-->
+{{-- <div class="row m-0">
     <div class="container mt-5 banner-product">
         <div class="title-banner"><img src="{{ url('chc/images/icon-thumbs-up.png') }}" alt="" class="icon-thumbs-up"> OUR PRODUCTS</div>
         @foreach($product_categories as $product_category)
             <a href="{{ url('product-category/'.$product_category->id) }}"><img src="{{ url('uploads/product-category/'.$product_category->image) }}" alt="{{ $product_category->name }}" class="img-fluid mb-5"></a>
         @endforeach
     </div>
-</div>
+</div> --}}
 <!--########################### END BANNER PRODUCT ###########################-->
 <!--########################## START BANNER PROMOTION ##########################-->
-<div class="row m-0">
+<div class="row mt-5">
     <div class="container mt-4">
         <div class="title-promotion"><span class="bg-title-promotion"><img src="{{ url('chc/images/icon-black-star.png') }}" alt="" class="icon-black-star"> PROMOTION</span></div>
         <hr class="line1">
