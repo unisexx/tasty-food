2a2effaaac1bf50613d118fe173e0c240266acee
unisexx <unisexx@gmail.com>
2020-11-18 09:33:45 +0700

    updated

--- resources/views/include/__box_product.blade.php
+++ resources/views/include/__box_product.blade.php
@@ -1,11 +1,14 @@
 <div class="col-12 col-sm-6 col-md-3 d-inline-block box-products">
     <p class="name-product">{{ @$product_item->name }}</p>
-    <img class="img-fluid" src="{{ url('uploads/product-item/'.@$product_item->productImageFirst->name) }}">
+    <a href="{{ url('product-item/'.@$product_item->id) }}">
+        <img class="img-fluid" src="{{ url('uploads/product-item/'.@$product_item->productImageFirst->name) }}">
+    </a>
     <div class="link">
         <ul>
             <li>
                 <div class="buy add1 item_add" data-id="{{ @$product_item->productItemPriceFirst->id }}" data-qty="1">
-                    <img src="{{ url('chc/images/icon-cart.png') }}" alt="" class="icon-buy"> <span class="text-white">Buy</span> <div class="carousel-control-next-icon i-buy"></div></a>
+                    <img src="{{ url('chc/images/icon-cart.png') }}" alt="" class="icon-buy"> 
+                    <span class="text-white">Buy</span> <div class="carousel-control-next-icon i-buy"></div></a>
                 </div>
             </li>
             <li> <a class="go-detail" href="{{ url('product-item/'.@$product_item->id) }}"><img src="{{ url('chc/images/icon-go-detail.png') }}" alt=""> Detail</a></li>
